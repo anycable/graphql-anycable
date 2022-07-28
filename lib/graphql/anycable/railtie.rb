935b0d1a23eb8cc7b2fd0c76764314a775605432
Genadi Samokovarov <gsamokovarov@gmail.com>
2022-07-28 16:01:36 +0300

    Better `config.use_client_provided_uniq_id` deprecation
    Moving this warning to the invocation of `GraphQL::AnyCable.use` makes
    it issuable outside of Rails.

--- lib/graphql/anycable/railtie.rb
+++ lib/graphql/anycable/railtie.rb
@@ -9,14 +9,6 @@ module GraphQL
         path = File.expand_path(__dir__)
         Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
       end
-
-      config.after_initialize do
-        if GraphQL::AnyCable.config.use_client_provided_uniq_id?
-          warn "[Deprecated] Using client provided channel uniq IDs could lead to unexpected behaviour, " \
-               "please, set GraphQL::AnyCable.config.use_client_provided_uniq_id = false or GRAPHQL_ANYCABLE_USE_CLIENT_PROVIDED_UNIQ_ID=false, " \
-               "and update the `#unsubscribed` callback code according to the latest docs."
-        end
-      end
     end
   end
 end
