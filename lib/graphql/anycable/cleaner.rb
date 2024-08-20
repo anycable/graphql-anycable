b45742ce1fa590fa4d20e3fe92d91d1811195f95
Ross-Hunter <ross-hunter@ross-hunter.com>
2024-08-20 15:11:41 -0400

    remove deprecation warnings from cleaner

--- lib/graphql/anycable/cleaner.rb
+++ lib/graphql/anycable/cleaner.rb
@@ -16,11 +16,13 @@ module GraphQL
         return unless config.subscription_expiration_seconds
         return unless config.use_redis_object_on_cleanup
 
-        redis.scan_each(match: "#{redis_key(adapter::CHANNEL_PREFIX)}*") do |key|
-          idle = redis.object("IDLETIME", key)
-          next if idle&.<= config.subscription_expiration_seconds
+        GraphQL::AnyCable.with_redis do |redis|
+          redis.scan_each(match: "#{redis_key(adapter::CHANNEL_PREFIX)}*") do |key|
+            idle = redis.object("IDLETIME", key)
+            next if idle&.<= config.subscription_expiration_seconds
 
-          redis.del(key)
+            redis.del(key)
+          end
         end
       end
 
@@ -28,31 +30,37 @@ module GraphQL
         return unless config.subscription_expiration_seconds
         return unless config.use_redis_object_on_cleanup
 
-        redis.scan_each(match: "#{redis_key(adapter::SUBSCRIPTION_PREFIX)}*") do |key|
-          idle = redis.object("IDLETIME", key)
-          next if idle&.<= config.subscription_expiration_seconds
+        GraphQL::AnyCable.with_redis do |redis|
+          redis.scan_each(match: "#{redis_key(adapter::SUBSCRIPTION_PREFIX)}*") do |key|
+            idle = redis.object("IDLETIME", key)
+            next if idle&.<= config.subscription_expiration_seconds
 
-          redis.del(key)
+            redis.del(key)
+          end
         end
       end
 
       def clean_fingerprint_subscriptions
-        redis.scan_each(match: "#{redis_key(adapter::SUBSCRIPTIONS_PREFIX)}*") do |key|
-          redis.smembers(key).each do |subscription_id|
-            next if redis.exists?(redis_key(adapter::SUBSCRIPTION_PREFIX) + subscription_id)
+        GraphQL::AnyCable.with_redis do |redis|
+          redis.scan_each(match: "#{redis_key(adapter::SUBSCRIPTIONS_PREFIX)}*") do |key|
+            redis.smembers(key).each do |subscription_id|
+              next if redis.exists?(redis_key(adapter::SUBSCRIPTION_PREFIX) + subscription_id)
 
-            redis.srem(key, subscription_id)
+              redis.srem(key, subscription_id)
+            end
           end
         end
       end
 
       def clean_topic_fingerprints
-        redis.scan_each(match: "#{redis_key(adapter::FINGERPRINTS_PREFIX)}*") do |key|
-          redis.zremrangebyscore(key, "-inf", "0")
-          redis.zrange(key, 0, -1).each do |fingerprint|
-            next if redis.exists?(redis_key(adapter::SUBSCRIPTIONS_PREFIX) + fingerprint)
+        GraphQL::AnyCable.with_redis do |redis|
+          redis.scan_each(match: "#{redis_key(adapter::FINGERPRINTS_PREFIX)}*") do |key|
+            redis.zremrangebyscore(key, "-inf", "0")
+            redis.zrange(key, 0, -1).each do |fingerprint|
+              next if redis.exists?(redis_key(adapter::SUBSCRIPTIONS_PREFIX) + fingerprint)
 
-            redis.zrem(key, fingerprint)
+              redis.zrem(key, fingerprint)
+            end
           end
         end
       end
@@ -61,10 +69,6 @@ module GraphQL
 
       def adapter
         GraphQL::Subscriptions::AnyCableSubscriptions
-      end
-
-      def redis
-        GraphQL::AnyCable.redis
       end
 
       def config
